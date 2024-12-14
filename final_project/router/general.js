59ebe0bd14be85779d9f7b71a3392a19325fc7fa
kimballmars <chenkimball@gmail.com>
2024-12-13 19:40:08 -0500

    task 1-3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,25 +7,45 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
+
+
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify(books,null,4));
+
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    let thisbook;
+    for (let key in books) {
+        if (books[key].isbn === isbn) {
+            thisbook = books[key];
+        }
+    }
+    
+    res.send(JSON.stringify(thisbook,null,4));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    let thosebooks = {};
+    let curNum = 1;
+    for (let keys in books) {
+        if (books[keys].author === author) {
+            thosebooks[curNum] = books[keys];
+            curNum += 1;
+        }
+    }
+    res.send(JSON.stringify(thosebooks,null,4));
 });
 
 // Get all books based on title
